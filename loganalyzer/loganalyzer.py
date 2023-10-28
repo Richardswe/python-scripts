fc3a7629a9245d21d99ba6f4e5f454bcf951eb57
Rickard <rickard.johan.a@protonmail.com>
2023-10-28 16:29:37 +0200

    added loganalyzer-script

--- loganalyzer/loganalyzer.py
+++ loganalyzer/loganalyzer.py
@@ -1,17 +1,17 @@
-# Author: Rickard Andersson 
-# 
-# Use the script to analyze log-files
-# In the terminal with the following order; python3 loganalyzer.py "filename" "action"
-
+# Rickard Andersson rickard.johan.a@protonmail.com
+# syntax in cli: python3 loganalyzer.py test.log statistics/error/notice
 import sys
 import re
+
+#   The variable to store the output
+statistics = []
 
 # Using the try statement to see if the arg ect are correct
 try:
     arg1_file = sys.argv[1]
     arg2_action = sys.argv[2]
     with open(arg1_file, "r") as file_object:
-        file = file_object.read()
+        file = file_object.readlines()
         actions = ["error", "notice", "statistics"]
         if arg2_action not in actions:
             raise IndexError
@@ -26,13 +26,20 @@ else:
             sub_str = "notice".lower()
             if line.lower().find(sub_str) != -1:
                 print(line.rstrip())
+    
     elif arg2_action == "error":
         for line in file:
             sub_str = "error".lower()
             if line.lower().find(sub_str) != -1:
-                print(line.rstrip()) 
+                print(line.rstrip())
     
     elif arg2_action == "statistics":
-        notices = file.count("[notice]")
-        errors = file.count("[error]")
-# print("Error: " + str(errors) + "\nNotice: " + str(notices))
+        for line in file:
+            statistics.append(line)
+            sub_s = "notice"
+            sub_s2 = "error"
+            res = [x for x in statistics if re.search(sub_s, x)]
+            res2 = [x for x in statistics if re.search(sub_s2, x)]
+            nl_1 = len(res)
+            nl_2 = len(res2)
+        print("Error: " + str(nl_2) + "\nNotice: " + str(nl_1))
