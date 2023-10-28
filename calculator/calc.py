0630bd0044b93aa05b6a19689bb9bf2eb3729021
Rickard <rickard.johan.a@protonmail.com>
2023-10-28 17:59:45 +0200

    added a fourth operatior

--- calculator/calc.py
+++ calculator/calc.py
@@ -2,9 +2,9 @@
 
 # Syntax in cli: python3 calc.py 
 
-var_number1 = int(input("Enter a number greater than 1: "))
-var_number2 = int(input("Enter a number greater than 1: "))
-oper = input("Choose a math operation (+, -, *): ")
+var_number1 = float(input("Enter a number greater than 1: "))
+var_number2 = float(input("Enter a number greater than 1: "))
+oper = input("Choose a math operation (+, -, *, /): ")
 while True:
     if oper == '+':
         print(var_number1, oper, var_number2, '=', var_number1 + var_number2)
@@ -14,6 +14,9 @@ while True:
         break
     elif oper == '*':
         print(var_number1, oper, var_number2, '=', var_number1 * var_number2)
+        break
+    elif oper == '/':
+        print(var_number1, oper, var_number2, '=', var_number1 / var_number2)
         break
     else:
         print('operator is not supported')
